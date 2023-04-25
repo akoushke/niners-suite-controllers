1dd0740fb9359a5f1abdc2b7d0468e6c5da055f1
Arash Koushkebaghi <arash.koush@gmail.com>
2023-04-25 12:20:59 -0700

    Updates

---
+++ _app/immutable/chunks/1.67a5ed86.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.1df11a17.js";export{t as component};
--- _app/immutable/chunks/1.a6816d7f.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.5561e0db.js";export{t as component};
