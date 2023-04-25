5112e08dda3fd3dd61ffbbf511c984b76411cae0
Arash Koushkebaghi <arash.koush@gmail.com>
2023-04-25 12:00:15 -0700

    more

--- gh-pages.js
+++ gh-pages.js
@@ -1,7 +1,7 @@
-var ghpages = require('gh-pages');
+import ghpages from 'gh-pages';
 
 ghpages.publish(
-  'public', // path to public directory
+  'build', // path to public directory
   {
     branch: 'gh-pages',
     repo: 'https://github.com/akoushke/niners-suite-controllers.git', // Update to point to your repository  
